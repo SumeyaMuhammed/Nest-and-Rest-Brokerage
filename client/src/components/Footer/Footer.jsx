e8bf71a8c24aa2d73865f52cd9d5bd6fab3ba704
Sumeya Muhammed <159241443+SumeyaMuhammed@users.noreply.github.com>
2024-12-26 06:10:28 -1100

    Add files via upload

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,41 +3,23 @@ import { FaLinkedinIn, FaFacebookF, FaTwitter, FaPaperPlane } from 'react-icons/
 const Footer = () => {
   return (
     <>
-    <footer>
-            <div className={`${classes.footerContent} ${classes.navbar}`}>
-                <div className={classes.logo}>
-                    <img
-                className={classes.logoimg}
-                src="/static/images/pngtree-github-icon-design-vector-png-image_5544222.png"
-                alt="logo"
-                width="35px"
-                />
-                    <a href="#">Nest & Ride</a>
-                </div>
-                <div className={classes.socialIcons}>
-      <a href="#">
-        <FaPaperPlane />
-      </a>
-      <a href="#">
-        <FaLinkedinIn />
-      </a>
-      <a href="#">
-        <FaTwitter />
-      </a>
-      <a href="#">
-        <FaFacebookF />
-      </a>
-    </div>
-
-    <div>
-        <p>Developed by:</p>
-
-    </div>
-            </div>
-    
-           
-        </footer>
-
+    <footer className={classes.footer}>
+      <div className={`${classes.footerContent} ${classes.navbar}`}>
+          <div className={classes.logo}>
+            <img className={classes.logoimg} src="/static/images/pngtree-github-icon-design-vector-png-image_5544222.png" alt="logo" width="35px" />
+            <a href="#">Nest & Ride</a>
+          </div>
+          <div className={classes.socialIcons}>
+            <a href="#"><FaPaperPlane /></a>
+            <a href="#"><FaLinkedinIn /></a>
+            <a href="#"><FaTwitter /></a>
+            <a href="#"><FaFacebookF /></a> 
+          </div>
+          <div>
+            <p>Developed by:</p>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
