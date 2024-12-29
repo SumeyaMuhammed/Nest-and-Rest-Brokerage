85e33f49f9de20456b79149cf145b023598fdf66
Sumeya Muhammed <159241443+SumeyaMuhammed@users.noreply.github.com>
2024-12-29 02:58:43 -1100

    Update house.routes.js

--- backend/routes/house.routes.js
+++ backend/routes/house.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const houseController = require('../controllers/house.controller');
+const { verifyToken, restrictTo } = require('../middleware/auth.middleware');
 const multer = require('multer');
 const path = require('path');
 
@@ -19,7 +20,7 @@ const upload = multer({ storage: storage });
 // Define house-related routes
 router.get('/', houseController.getAllHouses); // Get all houses
 router.get('/:id', houseController.getHouseById); // Get a specific house by ID
-router.post('/add',verifyToken, restrictTo(['admin']), upload.single('image'), houseController.addHouseWithImage); // Add a new house with image
+router.post('/',verifyToken, restrictTo(['admin']), upload.single('image'), houseController.addHouseWithImage); // Add a new house with image
 router.put('/update/:id',verifyToken, restrictTo(['admin']), upload.single('image'), houseController.updateHouseWithImage); // Update house details
 router.delete('/delete/:id',verifyToken, restrictTo(['admin']), houseController.deleteHouse); // Delete a house
 
