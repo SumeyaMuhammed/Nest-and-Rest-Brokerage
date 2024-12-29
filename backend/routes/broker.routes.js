06f1a1b5ce4d871a6093728f7cc47fe123305686
Sumeya Muhammed <159241443+SumeyaMuhammed@users.noreply.github.com>
2024-12-29 02:57:16 -1100

    Update broker.routes.js

--- backend/routes/broker.routes.js
+++ backend/routes/broker.routes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const brokerController = require('../controllers/broker.controller');
+const { verifyToken, restrictTo } = require('../middleware/auth.middleware');
 
 // Route to get all brokers
-router.get('/', brokerController.getAllBrokers);
+router.get('/',verifyToken, restrictTo(['admin']), brokerController.getAllBrokers);
 
 // Route to get a broker by ID
-router.get('/:id', brokerController.getBrokerById);
+router.get('/:id',verifyToken, restrictTo(['admin']), brokerController.getBrokerById);
 
 // Route to add a new broker
-router.post('/',verifyToken, restrictTo(['admin']), brokerController.addBroker);
+router.post('/', verifyToken, restrictTo(['admin']), brokerController.addBroker);
 
 // Route to update a broker
-router.put('/:id',verifyToken, restrictTo(['admin']), brokerController.updateBroker);
+router.put('/:id', verifyToken, restrictTo(['admin']), brokerController.updateBroker);
 
 // Route to delete a broker
-router.delete('/:id',verifyToken, restrictTo(['admin']), brokerController.deleteBroker);
+router.delete('/:id', verifyToken, restrictTo(['admin']), brokerController.deleteBroker);
 
 module.exports = router;
