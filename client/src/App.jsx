6049e1349d4affe8d69f1e7e99d3909862f4c626
Sumeya Muhammed <159241443+SumeyaMuhammed@users.noreply.github.com>
2025-01-05 20:18:42 -1100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,9 @@ import { AuthProvider } from "./context/AuthContext";
 // import AddCar from "./pages/Admin/AddCar/AddCar";
 // import AddBroker from "./pages/Admin/AddBroker/AddBroker";
 import { AdminRoutes } from "./utils/AdminRoutes";
+import ProtectedRoute from "./utils/ProtectedRoute"; 
+import GetInTouch from "./pages/GetInTouch/GetInTouch";
+import Services from "./pages/Services/Services";
 function App() {
   return (
     <AuthProvider>
@@ -18,10 +21,19 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
+        <Route path="/GetInTouch" element={<GetInTouch />} />
+        <Route path="/Services" element={<Services />} />
         {/* <Route> */}
           {AdminRoutes()}
         {/* </Route> */}
-        <Route path="/dashboard" element={<DashBoard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashBoard />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
       {/* <AddBroker/>
